c424ae416a4c718950a5abbc2437ffc1ecc11aa6
OrganomagnesiumHalide <99297401+OrganomagnesiumHalide@users.noreply.github.com>
2022-09-28 15:38:43 -0700

    Panic if not windows or linux

--- esp32/scripts/build_protobuf.py
+++ esp32/scripts/build_protobuf.py
@@ -2,11 +2,25 @@ import os
 from subprocess import check_output
 
 print(os.getcwd())
-os.chdir("../../protobufs")
-print(check_output(["nanopb_generator", "packet.proto"]))
-print(check_output(["mv", "packet.pb.c", "../esp32/src/generated"]))
-print(check_output(["mv", "packet.pb.h", "../esp32/src/generated"]))
+if os.name == 'posix':
+    os.chdir("../../protobufs")
+    print(check_output(["nanopb_generator", "packet.proto"]))
+    print(check_output(["mv", "packet.pb.c", "../esp32/src/generated"]))
+    print(check_output(["mv", "packet.pb.h", "../esp32/src/generated"]))
 
-print(check_output(["nanopb_generator", "FirmwareBackend.proto"]))
-print(check_output(["mv", "FirmwareBackend.pb.c", "../esp32/src/generated"]))
-print(check_output(["mv", "FirmwareBackend.pb.h", "../esp32/src/generated"]))
+    print(check_output(["nanopb_generator", "FirmwareBackend.proto"]))
+    print(check_output(["mv", "FirmwareBackend.pb.c", "../esp32/src/generated"]))
+    print(check_output(["mv", "FirmwareBackend.pb.h", "../esp32/src/generated"]))
+elif os.name == 'nt':
+    os.chdir("../../protobufs")
+    print(check_output(["nanopb_generator", "packet.proto"]))
+    print(check_output(["move", "packet.pb.c", "../esp32/src/generated"]))
+    print(check_output(["move", "packet.pb.h", "../esp32/src/generated"]))
+
+    print(check_output(["nanopb_generator", "FirmwareBackend.proto"]))
+    print(check_output(["move", "FirmwareBackend.pb.c", "../esp32/src/generated"]))
+    print(check_output(["move", "FirmwareBackend.pb.h", "../esp32/src/generated"]))
+else:
+    print("What is {}".format(os.name))
+    exit(-1)
+
