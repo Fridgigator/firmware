1c920742ab25bd10fa96c88d542bbf39251c62d8
OrganomagnesiumHalide <99297401+OrganomagnesiumHalide@users.noreply.github.com>
2023-02-03 13:05:58 -0800

    Changed destination location

--- esp32/src/Constants.h
+++ esp32/src/Constants.h
@@ -13,7 +13,7 @@ extern safe_std::mutex<std::map<std::string, SensorDataStore>> sensorData;
 
 #define CHARACTERISTIC_SERVER_UUID "2630acab-7bf5-4dee-97fb-af8d3955c2aa"
 #define SERVICE_UUID "170e6a4c-af9e-4a1f-843e-e4fb5e165c62"
-#define REMOTE_HOST "http://ec2-3-84-82-97.compute-1.amazonaws.com"
-#define REMOTE_HOST_WS "ws://ec2-3-84-82-97.compute-1.amazonaws.com"
+#define REMOTE_HOST "http://detoirhbf2f8n.cloudfront.net";
+#define REMOTE_HOST_WS "ws://detoirhbf2f8n.cloudfront.net"
 #define NTP_SERVER "time.cloudflare.com"
 #endif //ESP32_CONSTANTS_H
